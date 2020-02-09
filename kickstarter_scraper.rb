dd2dc01e252f002ebb423aa55c8c7cdf90c091d4
Kyle McKenna <mckennakyler@gmail.com>
2020-02-09 21:17:56 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,11 +5,13 @@ require 'pry'
 
 def create_project_hash
   
+  projects = {}
+  
   html = File.read('fixtures/kickstarter.html')
   
   kickstarter = Nokogiri::HTML(html)
   
-  projects = {}
+  
   
   #Iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project|
