bc2d3115f1f61cc232e3cd667ab5ffc08f3edeaa
Kyle McKenna <mckennakyler@gmail.com>
2020-02-09 21:17:06 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,7 +14,12 @@ def create_project_hash
   #Iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project|
     projects[project] = {}
+  end 
   
+  #return the projects hash 
+  projects 
+
+end   
   #binding.pry 
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
@@ -22,7 +27,5 @@ def create_project_hash
 # description: project.css("p.bbcard_blurb").text  
 # location: project.css("ul.project-meta span.location-name")
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-
-end
 
 create_project_hash
